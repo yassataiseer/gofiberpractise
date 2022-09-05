64a8f9fc3aeefe0d09e4b78b48b643e471cce559
Yassa Taiseer <yassataiseer@gmail.com>
2022-09-05 15:34:34 -0400

    comit

---
+++ endpoints/api.go
@@ -0,0 +1,19 @@
+package endpoints
+
+import (
+    "github.com/gofiber/fiber/v2"
+)
+
+
+
+func Admin(c *fiber.Ctx) error {
+
+	return c.JSON(&fiber.Map{
+		"success": true,
+		"ADMIN":   "LOGGED IN",
+	})
+}
+func Register(app *fiber.App) {
+  app.Get("/admin",  Admin)
+
+}
